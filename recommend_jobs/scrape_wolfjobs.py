5c79e209fed4fc38972095d7cf7917f3a666c3bb
Sweekar Burji <sweekarvb@gmail.com>
2024-11-19 12:09:46 -0500

    Fetch Open Positions

--- recommend_jobs/scrape_wolfjobs.py
+++ recommend_jobs/scrape_wolfjobs.py
@@ -7,8 +7,24 @@ app = Flask(__name__)
 CORS(app)
 
 def fetch_wolfjobs():
-    url = ""
-    name = ""
-    link = ""
-    return name, link
+    url = "https://campusenterprises.ncsu.edu/dept/hr/opportunities/student/jobs/"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        tree = html.fromstring(response.content)
+        positions = tree.xpath('//a[@class="ce-jazzhr-job"]')
+        total_listings = len(positions)
+        departments = tree.xpath('//a[@class="ce-jazzhr-dept"]')
+        total_dept = len(departments)
+
+        print(total_listings, total_dept)
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        raise Exception(f"Error fetching data: {str(e)}")
+    
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        raise Exception(f"Unexpected error: {str(e)}")
         
+fetch_wolfjobs()
