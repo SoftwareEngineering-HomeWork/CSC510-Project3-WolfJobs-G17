31c8ce5943c7635dd1fe5a3f7446ad0252cb1552
Sweekar Burji <sweekarvb@gmail.com>
2024-11-25 19:46:09 -0500

    Update Scrape_Wolfjobs script

--- recommend_jobs/scrape_wolfjobs.py
+++ recommend_jobs/scrape_wolfjobs.py
@@ -10,29 +10,35 @@ def fetch_wolfjobs():
     url = "https://campusenterprises.ncsu.edu/dept/hr/opportunities/student/jobs/"
     try:
         response = requests.get(url)
-        response.raise_for_status()
-        tree = html.fromstring(response.content)
-        listings = tree.xpath('//a[@class="ce-jazzhr-job"]')
-        total_listings = len(listings)
-        dept = tree.xpath('//a[@class="ce-jazzhr-dept"]')
+        response.raise_for_status() 
         
-        i=0
-        positions = []
-        links = []
-        departments = []
+        if response.content:  # Check if content is not empty
+            tree = html.fromstring(response.content)
+            listings = tree.xpath('//a[@class="ce-jazzhr-job"]')
+            total_listings = len(listings)
+            dept = tree.xpath('//a[@class="ce-jazzhr-dept"]')
 
-        while i<total_listings:
-            positions.append(listings[i].text_content().strip())
-            links.append(listings[i].get('href').strip())
-            departments.append(dept[i].text_content().strip())
-            i += 1
-        
-        return positions,links,departments
-    
+            positions = []
+            links = []
+            departments = []
+
+            for i in range(total_listings):
+                positions.append(listings[i].text_content().strip())
+                link = listings[i].get('href')
+                if link: 
+                    links.append(link.strip())
+                else:
+                    links.append("") 
+                
+                if i < len(dept):
+                    departments.append(dept[i].text_content().strip())
+
+            return positions, links, departments
+
     except requests.exceptions.RequestException as e:
         print(f"Error fetching data: {e}")
-        raise Exception(f"Error fetching data: {str(e)}")
-    
+        raise e 
+
     except Exception as e:
         print(f"Unexpected error: {e}")
         raise Exception(f"Unexpected error: {str(e)}")
